219745984f79dbc44f57307234526195483a6d48
Tidalfeer <tylerlee1406@gmail.com>
2024-10-04 11:16:26 -0400

    Add files via upload

---
+++ L3script.js
@@ -0,0 +1,39 @@
+const myArray = [64, 'Wizard', 'Cleric', 25, 'Bard'];
+console.log("Array declared: ", myArray);
+
+const firstElement = myArray[0];
+const lastElement = myArray[myArray.length - 1];
+const middleElement = myArray[Math.floor(myArray.length / 2)];
+
+console.log("First Element: ", firstElement);
+console.log("Last Element: ", lastElement);
+console.log("Middle Element:", middleElement);
+
+const arraylenght = myArray.length;
+console.log("Array Length: ", arraylenght);
+
+myArray.push('newElement');
+console.log("After push: ", myArray);
+
+myArray.pop();
+console.log("After pop:" ,myArray);
+
+myArray.unshift('startElement');
+console.log("After unshift: ", myArray);
+
+myArray.splice(2,1, 'modifiedElement');
+console.log("After splice ",myArray);
+
+for(let x = 0; x < myArray.length; x++) {
+    console.log("Element at index", x, "is", myArray[x]);
+}
+
+myArray.forEach((element, index) => {
+    console.log("forEach element at index", index, "is", element);
+});
+
+const [first, second,...rest] = myArray;
+console.log("First element via destructuring: ", first);
+console.log("Second element via destructuring: ", second);
+console.log("Remaining elements via rest: ", rest);
+
